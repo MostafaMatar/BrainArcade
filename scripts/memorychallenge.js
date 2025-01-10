561ce71fa9f7668606a4e274a76746fdf7d75c47
Mostafa Matar <engmostafamatar@gmail.com>
2025-01-10 13:39:33 +0100

    Add icon and other changes

--- scripts/memorychallenge.js
+++ scripts/memorychallenge.js
@@ -1,53 +1,72 @@
-const cardValues = ['Dog', 'Apple', 'Paris', 'Daisy', 'Ford', 'Star', 
-                    'Yoda', 'Tree', 'Frog', 'Moon', 'Magic', 'Gold',
-                    'Baby', 'Shark', 'Snake', 'Drama', 'Ruby', 'Cairo',
-                    'Witch', 'Salt', 'Fiji', 'Clay', 'Dark', 'Zink'
-                  ]; 
-cardValues.sort(() => Math.random() - 0.5); 
+const cardValues = ['Dog', 'Apple', 'Paris', 'Daisy', 'Ford', 'Star',
+  'Yoda', 'Tree', 'Frog', 'Moon', 'Magic', 'Gold',
+  'Baby', 'Shark', 'Snake', 'Drama', 'Ruby', 'Cairo',
+  'Witch', 'Salt', 'Fiji', 'Clay', 'Dark', 'Zink'
+];
+cardValues.sort(() => Math.random() - 0.5);
 
-const gameBoard = document.getElementById('game-board'); 
+const gameBoard = document.getElementById('game-board');
+const results = document.getElementById('results');
+const newGameButton = document.getElementById('new-game-button');
+newGameButton.addEventListener('click', () => {
+  window.location.reload();
+});
 
-let flippedCards = []; 
-let matchedCards = []; 
+let flippedCards = [];
+let matchedCards = [];
 let cards = [];
+
+const timerDisplay = document.getElementById('timer-display');
+let time = 0;
+
+let intervalId = setInterval(function () {
+  time++;
+  timerDisplay.textContent = 'Time: ' + time;
+}, 1000);
 
 for (let i = 0; i < 12; i++) {
   const card = document.createElement('div');
   card.classList.add('card');
-  card.dataset.value = cardValues[Math.floor(i/2)];
-  card.textContent = '?'; 
-  card.addEventListener('click', flipCard); 
+  card.dataset.value = cardValues[Math.floor(i / 2)];
+  card.textContent = '?';
+  card.addEventListener('click', flipCard);
   cards.push(card);
 }
-cards.sort(() => Math.random() - 0.5); 
+cards.sort(() => Math.random() - 0.5);
 cards.forEach((card) => gameBoard.appendChild(card));
 
 function flipCard(event) {
-  const card = event.target; 
-  card.textContent = card.dataset.value; 
-  if(!flippedCards.includes(card)){
-    flippedCards.push(card); 
-  } 
+  const card = event.target;
+  if (!matchedCards.includes(card)) {
+    card.textContent = card.dataset.value;
+    if (!flippedCards.includes(card)) {
+      flippedCards.push(card);
+    }
 
-  if (flippedCards.length === 2) {
-    checkForMatch(); 
-  } 
-} 
+    if (flippedCards.length === 2) {
+      checkForMatch();
+    }
+  }
+}
 
 function checkForMatch() {
-  const card1 = flippedCards[0]; 
-  const card2 = flippedCards[1]; 
+  const card1 = flippedCards[0];
+  const card2 = flippedCards[1];
 
   if (card1.dataset.value === card2.dataset.value) {
-    matchedCards.push(card1, card2); 
-    card1.classList.add('match'); 
-    card2.classList.add('match'); 
-    flippedCards = []; 
+    matchedCards.push(card1, card2);
+    card1.classList.add('match');
+    card2.classList.add('match');
+    flippedCards = [];
+    if(matchedCards.length === 12){
+      //game is won
+      results.innerHTML = `<p>You win! You matched all cards in ${time} seconds.`;
+      clearInterval(intervalId);
+    }
   } else {
     setTimeout(() => {
-      card1.textContent = '?'; 
-      card2.textContent = '?'; 
-      flippedCards = []; 
-    }, 1000); 
-  } 
+      flippedCards.forEach((c) => c.textContent = '?');
+      flippedCards = [];
+    }, 1000);
+  }
 }
