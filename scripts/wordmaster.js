af316350eb0bfff5a8bcf25b74726ccc5ac03a44
Mostafa Matar <engmostafamatar@gmail.com>
2025-01-15 20:39:19 +0100

    enable submission via keyboard

--- scripts/wordmaster.js
+++ scripts/wordmaster.js
@@ -3,6 +3,13 @@ const guessInput = document.getElementById('guess');
 const submitButton = document.getElementById('submit');
 const feedbackElement = document.getElementById('feedback');
 
+guessInput.addEventListener('keyup', function (event) {
+  if (event.key === 'Enter') {
+    checkGuess(event.target.value);
+    event.target.value = '';
+  }
+});
+
 let words = [
   "apple", "banana", "orange", "grapefruit", "watermelon", "strawberry", "blueberry", "raspberry", "mango", "pineapple", "carrot", "broccoli", "spinach", "tomato", "cucumber", "onion", "potato", "garlic", "ginger", "lemon", "lime", "avocado", "pear", "peach", "plum", "apricot", "kiwi", "grape", "fig", "melon", "papaya", "coconut", "banana", "mango", "pineapple", "strawberry", "blueberry", "raspberry", "cherry", "plum", "apple", "orange", "grapefruit", "watermelon", "kiwi", "pear", "peach", "apricot", "fig", "date", "pomegranate", "london", "paris", "tokyo", "rome", "madrid", "berlin", "moscow", "cairo", "sydney", "toronto", "table", "chair", "bed", "sofa", "lamp", "television", "computer", "phone", "book", "pen", "pencil", "paper", "keyboard", "mouse", "monitor", "printer", "car", "truck", "bus", "train", "airplane", "boat", "house", "apartment", "school", "hospital", "park", "library", "museum", "zoo", "aquarium", "planet", "star", "sun", "moon", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto", "galaxy", "universe", "cloud", "rain", "sun", "wind", "snow", "fire", "water", "earth", "air", "tree", "flower", "grass", "animal", "bird", "fish", "insect", "rock", "stone", "mountain", "river", "lake", "ocean", "desert", "forest", "jungle", "beach", "city", "country", "continent", "world"
 ]; 
